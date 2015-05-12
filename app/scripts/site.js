6366e11799a5d8db8e648c7132856bb4eb4b5fba
Piermaria Cosina <piermaria@belafonte.co>
2015-05-12 23:33:32 +0200

    bimini fix

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -140,6 +140,11 @@ $(document).ready(function() {
     planSetup();
     getPlanPins();
 
+    $('#plan-modal').on($.modal.CLOSE, function(event, modal) {
+      console.log("Pins cleaned")
+      cleanPins();
+    });
+    
     $(document).keydown(function(e) {
     switch(e.which) {
         case 83: // up
@@ -176,6 +181,8 @@ $(document).ready(function() {
     filterMenu();
     rangeSlider();
     guestSlider();
+
+
     $( "#slider-single" ).slider({
          min: 0,
          max: 2,
@@ -739,7 +746,15 @@ function hidePrivateSection(){
 // };
 
 
-//
+$('#plan-modal').on($.modal.CLOSE, function(event, modal) {
+  cleanPins();
+});
+
+function cleanPins(){
+    $("#sundeck").find(".dot").remove();
+    $("#maindeck").find(".dot").remove();
+    $("#lowerdeck").find(".dot").remove();
+}
 function getPlanPins(){
     $(document).on('click', 'a.see-plan', function(e) {
             var deck = $(this).attr("data-deck");
